ca3afbccfe33a68791f6821aacc1fca6ae802ba3
chigozieokoroafor <48325147+chigozieokoroafor@users.noreply.github.com>
2021-12-21 12:29:47 -1100

    made the texts show
    made notes specific to user show on the webpage

--- todo_folder_new/todo.py
+++ todo_folder_new/todo.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, Response, render_template, jsonify, session, redirect,url_for
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user 
+
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 #from bson import ObjectId
 #import uuid
 #import jwt
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from functools import wraps
 import pymongo
 import json
@@ -18,6 +18,7 @@ mongo = pymongo.MongoClient(host="localhost", port=27017, serverSelectionTimeout
 mongo.server_info()
 db = mongo.todo_users
 User  = db.users
+Notes = db.notes
 #login_manager = LoginManager()
 
 app = Flask(__name__)
@@ -33,42 +34,63 @@ jwt = JWTManager(app)
 
 
 
-@app.route("/home")
-@app.route("/")
-#@jwt_required
+@app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if session["logged_in"]==True:
-        #return ("welcome " + session["username"], 200)
-        access = {"username":session["username"]}
-        access_token = create_access_token(identity=access)
-        return Response(json.dumps({"message": f"user created for {session['username']}", "status":"Success", "token":access_token}))
-    return jsonify({"message": "please login", "status":"neutral"})
+    #if "username" in session :
+    try:
+        if session["logged_in"]==True:
+            #return ("welcome " + session["username"], 200)
+            access = {"username":session["username"]}
+            access_token = create_access_token(identity=access)
+            if request.method == "POST":
+                note = request.form.get("notes")
+                note_dict = {"email":User.find_one({"username":session["username"]})["email"], "notes":note}
+                Notes.insert_one(note_dict)
+            
+                note_list = []
+                for note_ in Notes.find({"email":User.find_one({"username":session["username"]})["email"]}):
+                    #return (note_["notes"])
+                    note_list.append(note_["notes"])
+                #return jsonify(note_list)
+                return render_template("home.html", template_folder="templates", tasks=note_list)
+            return render_template("home.html", template_folder="templates", token=access_token)
+            #return Response(json.dumps({"message": "welcome"+" "+f"{session['username']}", "status":"Success", "token":access_token}))
+        
+        else:
+            return render_template("home.html", template_folder="templates")
+            #return jsonify({"message": "please login", "status":"neutral"})
+    except:
+        #return render_template('home.html')
+        return "an error occured"
+    
 
 
 
-@app.route("/signin", methods = ["POST"])
+@app.route("/signin", methods = ["GET", "POST"]) 
 #login_manager.request_loader
 def signin():
-    email = request.form.get("email")
-    password = request.form.get("password")
-        
-    check_user = User.find_one({"email":email})
-    if check_user: 
-        if check_password_hash(check_user["password"],password):
-            #return Response(json.dumps({"message":"continue with this id", "status":"success"}), 200)
-            session["username"] = check_user["username"]
-            #login_user(check_user)
-            #print(check_user["username"])
-            #message = json.dumps({"message":("welcome" + check_user["username"]), "status":"success"})
-            #return (message)
-            access_token = create_access_token(identity={"email":email})
-            session["logged_in"] = True
-            return json.dumps({"message": "welcome"+" "+check_user["username"], "status":"Success", "token":access_token})
-        password_message = json.dumps({"message":"incorrect password", "status":"error"})
-        return (password_message)
-    message = json.dumps({"message":"user not found", "status":"error"})
-    return message
-    #return redirect(url_for("signin"))            
+    if request.method == "POST":
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+            
+        check_user = User.find_one({"email":email})
+        if check_user: 
+            if check_password_hash(check_user["password"],password):
+                session["username"] = check_user["username"]
+                access_token = create_access_token(identity={"email":email})
+                session["logged_in"] = True
+                #return json.dumps({"message": "welcome"+" "+check_user["username"], "status":"Success", "token":access_token})
+                return redirect(url_for("home"))            
+            password_message = json.dumps({"message":"incorrect password", "status":"error"})
+            return (password_message)
+        message = json.dumps({"message":"user not found", "status":"error"})
+        return message
+
+        #return redirect(url_for("signin"))            
+    return render_template("signin.html", template_folder="templates")
+    #return Response("please signin using postman", 200)
             
 
 
@@ -79,22 +101,27 @@ def signup():
         username = request.form.get("username")
         password = request.form.get("password")
         time = datetime.utcnow()
+        
         hashed = generate_password_hash(password)
         new_user = {"email":email, "username": username, "password":hashed, "time created":time}
         access = {"email":email, "password":password}
         dbResponse = db.users.insert_one(new_user)
         access_token = create_access_token(identity={"email":email})
         session["logged_in"] = True
-        return json.dumps({"message": "user created", "status":"Success", "token":access_token})
-        #login_user(access)
-    #return render_template("signup.html")
+        #return json.dumps({"message": "user created", "status":"Success", "token":access_token})
+        return redirect(url_for(endpoint="home"))
+        
+    return render_template("signup.html")
 
 @app.route("/logout")
 def logout():
     if "username" in session:
         session["logged_in"] = False
-#        return redirect(url_for("login"))
-    return home()
+        return redirect(url_for(endpoint ="home"))
+    else:
+        return "no user in session", 401
+    #return home()
+    
 
 @app.route("/test")
 @jwt_required()
@@ -102,6 +129,7 @@ def test():
     user_jwt = get_jwt_identity()
     print(user_jwt)
     return "Secret Data", 200
+    
 
 
 
